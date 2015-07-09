490c79db6f872aaeb5422d7eb7449ad98a73e9eb
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-07-10 00:25:48 +0530

    Added nameof operator code.

--- LatestCSharpFeatures/NameOfOperator/Program.cs
+++ LatestCSharpFeatures/NameOfOperator/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NameOfOperator
 {
@@ -10,6 +6,13 @@ namespace NameOfOperator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(nameof(Employee));
+            Console.ReadKey();
         }
+    }
+
+    public class Employee
+    {
+        
     }
 }
