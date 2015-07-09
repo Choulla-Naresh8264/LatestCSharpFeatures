90a9b6b66bbb15bfb5b54e0cabdcab68b533f72c
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-07-10 00:02:20 +0530

    Added Auto Property Intializers.

---
+++ LatestCSharpFeatures/AutoPropertyIntializers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AutoPropertyIntializers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Employee
+            {
+                FirstName ="Jalpesh",
+                LastName ="Vadgama"
+            };
+
+            Console.WriteLine("Printing Employee");
+            Console.WriteLine(employee.Id);
+            Console.WriteLine(employee.FirstName);
+            Console.WriteLine(employee.LastName);
+            
+        }
+    }
+
+    public class Employee
+    {
+        public Guid Id {get;set;}= Guid.NewGuid();
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+    }
+}
