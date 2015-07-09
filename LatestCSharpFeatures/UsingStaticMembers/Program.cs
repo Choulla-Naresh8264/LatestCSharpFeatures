8259894fa27a3a5f5cafbfa2fc2fad42a7771d41
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-07-10 00:07:32 +0530

    Added using static Member Project.

---
+++ LatestCSharpFeatures/UsingStaticMembers/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+namespace UsingStaticMembers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Printing this with using static features");
+            ReadKey();
+        }
+    }
+}
