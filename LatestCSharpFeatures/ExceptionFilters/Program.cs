3c2dbaf72e5224f80f95da0a4e917f5d18641622
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-07-10 00:13:17 +0530

    Added Exception filters console application.

---
+++ LatestCSharpFeatures/ExceptionFilters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExceptionFilters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new Exception("My Exception");
+            }
+            catch (Exception ex) when (ex.Message == "Another message")
+            {
+                Console.WriteLine("Another message exception");
+            }
+            catch (Exception ex) when (ex.Message == "My Exception")
+            {
+                Console.WriteLine("My Exception exception");
+            }
+            Console.ReadKey();
+        }
+    }
+}
