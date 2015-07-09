fe6ad0229865fe38b088650ac93d43af1040851b
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-07-10 00:23:11 +0530

    Added string interpolation code.

--- LatestCSharpFeatures/StringInterPolation/Program.cs
+++ LatestCSharpFeatures/StringInterPolation/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StringInterPolation
 {
@@ -10,6 +6,16 @@ namespace StringInterPolation
     {
         static void Main(string[] args)
         {
+            Student student = new Student();
+            Console.Write($"First Name:{student.FirstName}");
+            Console.Write($"Last Name:{student.LastName}");
         }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }=1;
+        public string FirstName {get;set;}="Jalpesh";
+        public string LastName {get;set;}="Vadgama";
     }
 }
